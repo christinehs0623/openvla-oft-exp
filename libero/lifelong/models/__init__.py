a8c188db94e45c08b2678c5914d106e7a168a59a
Tzu-Tsen Hsieh <yammy0623@gmail.com>
2026-05-10 09:46:13 +0800

    init

---
+++ libero/lifelong/models/__init__.py
@@ -0,0 +1,5 @@
+from openvla.libero.lifelong.models.bc_rnn_policy import BCRNNPolicy
+from openvla.libero.lifelong.models.bc_transformer_policy import BCTransformerPolicy
+from openvla.libero.lifelong.models.bc_vilt_policy import BCViLTPolicy
+
+from openvla.libero.lifelong.models.base_policy import get_policy_class, get_policy_list
